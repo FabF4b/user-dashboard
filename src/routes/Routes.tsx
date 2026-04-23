de9f044698823daca44b6a5fe1a17d0dc5e42844
FabFab <fabian@nvfab.de>
2026-04-23 13:48:49 +0200

    Add deploy and update routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,17 +4,20 @@ import Overview from "../page/components/overview/Overview";
 import CreateNewUser from "../page/components/newUser/NewUser";
 import NotFound from "../page/components/notFound/NotFound";
 
-export const router = createBrowserRouter([
-  {
-    path: "/user-dashboard",
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Overview /> },
-      {
-        path: "create",
-        element: <CreateNewUser />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/user-dashboard",
+      element: <App />,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <Overview /> },
+        {
+          path: "create",
+          element: <CreateNewUser />,
+        },
+      ],
+    },
+  ],
+  { basename: "/user-dashboard/" },
+);
